0f6a2ccf216d8304b510015651b4132cc4e34d46
mebangit123 <mebannongrum333@gmail.com>
2021-04-15 13:44:10 +0530

    Compare two lines by applying oops concept

--- LineComparison.java
+++ LineComparison.java
@@ -2,13 +2,19 @@ public class LineComparison {
 public static void main(String[] args) {
 	System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
 
-	int x1=3, x2=4, y1=6, y2=5;
-	int x3=3, x4=4, y3=6, y4=5;
+	Line line = new Line();
 
-	String line1 = Double.toString(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 -y1, 2)));
-	String line2 = Double.toString(Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 -y3, 2)));
+	String line1 = Double.toString(line.computelength(1, 2, 3, 5));
+	String line2 = Double.toString(line.computelength(1, 2, 3, 4));
 
 	System.out.println(line1.equals(line2));
 	System.out.println(line1.compareTo(line2));
+	}
+}
+
+class Line {
+public double computelength(int x1, int y1, int x2, int y2){
+	double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+	return length;
 	}
 }
